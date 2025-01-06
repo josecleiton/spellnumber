99ee64dafd8da3c231296bf3c762161e5f4c02ef
José Cleiton Correia <jcleitonbc@gmail.com>
2025-01-05 21:24:51 -0300

    test analex

---
+++ analex_test.go
@@ -0,0 +1,96 @@
+package spellnumber
+
+import (
+	"testing"
+)
+
+func TestLexerParseLine(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected []Token
+	}{
+		{
+			name:  "Simple number",
+			input: "cem",
+			expected: []Token{
+				{Type: TOKEN_NUMBER_PARSED, Value: "100"},
+			},
+		},
+		{
+			name:  "Multiple numbers",
+			input: "cento e vinte",
+			expected: []Token{
+				{Type: TOKEN_NUMBER_PARSED, Value: "120"},
+			},
+		},
+		{
+			name:  "Fatorial",
+			input: "fatorial de três",
+			expected: []Token{
+				{Type: TOKEN_FACTORIAL, Value: "!"},
+				{Type: TOKEN_NUMBER_PARSED, Value: "3"},
+			},
+		},
+		{
+			name:  "Operador",
+			input: "trinta e seis vezes dois",
+			expected: []Token{
+				{Type: TOKEN_NUMBER_PARSED, Value: "36"},
+				{Type: TOKEN_TIMES, Value: "*"},
+				{Type: TOKEN_NUMBER_PARSED, Value: "2"},
+			},
+		},
+		{
+			name:  "Invalid input",
+			input: "abc",
+			expected: []Token{
+				{Type: TOKEN_ERROR, Value: "abc", Spell: "Invalid input"},
+			},
+		},
+		{
+			name:  "Invalid 'cento'",
+			input: "cento",
+			expected: []Token{
+				{Type: TOKEN_ERROR},
+				{Type: TOKEN_NUMBER_PARSED, Value: "100"},
+			},
+		},
+		{
+			name:  "Parentese",
+			input: "abre parentese dois fecha parentese",
+			expected: []Token{
+				{Type: TOKEN_LEFT_BRACKET, Value: "("},
+				{Type: TOKEN_NUMBER_PARSED, Value: "2"},
+				{Type: TOKEN_RIGHT_BRACKET, Value: ")"},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			lexer := NewLexer(nil)
+			tokens, err := lexer.ParseLine(test.input)
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+
+			if len(tokens) != len(test.expected) {
+				t.Errorf("expected %d tokens, got %d", len(test.expected), len(tokens))
+			}
+
+			for i, token := range tokens {
+				if token.Type != test.expected[i].Type {
+					t.Errorf("expected token type %v, got %v", test.expected[i].Type, token.Type)
+
+					if token.Type == TOKEN_ERROR {
+						continue
+					}
+				}
+				if token.Value != test.expected[i].Value {
+					t.Errorf("expected token value %v, got %v", test.expected[i].Value, token.Value)
+				}
+			}
+		})
+	}
+}
